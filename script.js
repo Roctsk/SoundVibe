118d9b3fa89f4dbff202a57d968288c781b96d6a
Roctsk <rostyk20166@gmail.com>
2025-04-03 21:50:41 +0300

    i debag the bag

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const registerButton = document.getElementById("register");
 const loginButton = document.getElementById("login");
 const container = document.getElementById("container");
+const bgMusic = document.getElementById("bg-music")
 
 registerButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
@@ -8,4 +9,13 @@ registerButton.addEventListener("click", () => {
 
 loginButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
+});
+
+document.addEventListener("DOMContentLoaded", () =>{
+  bgMusic.play();
+});
+
+window.addEventListener("beforeunload", () =>{
+  bgMusic.pause();
+  bgMusic.currentTime = 0;
 });
